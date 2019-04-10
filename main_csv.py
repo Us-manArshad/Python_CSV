b44d612f780647fc19572b6ec83bcfcebf663228
Us-manArshad <Usmangani908.ua@gmail.com>
2019-04-10 00:59:42 -0700

    started a project that shows the highest temp and its date

---
+++ main_csv.py
@@ -0,0 +1,70 @@
+import os
+import csv
+from datetime import datetime
+
+
+class Weather:
+
+    def read(self, path, filename):
+        size  = []
+        with open(path) as input_file:
+            reader = csv.reader(input_file)
+            lines = input_file.readlines()
+            for line in lines[2:]:
+                if line != '\n' and line.find('<') < 0:
+                    row = line.strip().split(',')
+                    if row[1] != '':
+                        size.append(int(row[1]))
+                #find max from li size        
+                maximum = self.find_max(size)
+                index_list = []
+                # it will print number indexes where maximum values are founds
+                index_list = self.find_index(size)
+                # It will print date of indexes
+                index_date = []
+                for i in range(len(index_list)):
+                    index_date.append(str(self.find(index_list[i], path)))
+        
+            return "Maximum Temp of month:",filename," is: ",maximum, "at Dates: ", index_date
+
+
+    def find(self, index, path):
+        li,li1  = [],[]
+        with open(path) as input_file:
+            reader = csv.reader(input_file)
+            lines = input_file.readlines()
+            for line in lines[2:]:
+                if line != '\n' and line.find('<') < 0:
+                    row = line.strip().split(',')
+                    row_date = datetime.strptime(row[0], "%Y-%m-%d")
+                    li.append(row_date.strftime("%Y-%m-%d"))
+        val = li[index]    
+        return val
+
+
+    def find_max(self,li):
+        maximum = 0
+        for i in range(len(li)):
+            if maximum <= li[i]:
+                maximum = li[i]
+        return maximum
+
+    
+    def find_index(self, li):
+        Maximum = self.find_max(li)
+        index_list = []
+        for i,j in enumerate(li):
+            if j == Maximum:
+                index_list.append(i)
+        return index_list
+    
+
+if __name__ == "__main__":
+    weather = Weather()        
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(ROOT_DIR, 'csv')
+    for filename in os.listdir(DATA_DIR):
+        file = os.path.join(DATA_DIR, filename)
+        print(weather.read(file, filename))
+        
+
