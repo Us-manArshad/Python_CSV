759af345cf4ecf155b6c2d5afe23f202d0b487fd
Us-manArshad <Usmangani908.ua@gmail.com>
2019-04-10 22:56:17 -0700

    Fixed some bugs

--- main_csv.py
+++ main_csv.py
@@ -1,14 +1,13 @@
 import os
-import csv
 from datetime import datetime
 
 
 class Weather:
 
+
     def read(self, path, filename):
         size  = []
         with open(path) as input_file:
-            reader = csv.reader(input_file)
             lines = input_file.readlines()
             for line in lines[2:]:
                 if line != '\n' and line.find('<') < 0:
@@ -16,7 +15,6 @@ class Weather:
                     if row[1] != '':
                         size.append(int(row[1]))
                 #find max from li size
-                
                 maximum = self.find_max(size)
                 index_list = []
                 # it will print number indexes where maximum values are founds
@@ -24,16 +22,35 @@ class Weather:
                 # It will print date of indexes
                 index_date = []
                 for i in range(len(index_list)):
-                    index_date.append(str(self.find(index_list[i], path)))
+                    index_date.append(str(self.find_date(index_list[i], path)))
                 
-            print("Maximum Temp of month:",filename," is: ",maximum, "at Dates: ", index_date)
-        return maximum
+        return index_date, filename
 
+
+    def read_file(self, path, filename):
+        size  = []
+        with open(path) as input_file:
+            lines = input_file.readlines()
+            for line in lines[2:]:
+                if line != '\n' and line.find('<') < 0:
+                    row = line.strip().split(',')
+                    if row[1] != '':
+                        size.append(int(row[1]))
+                #find max from li size
+                maximum = self.find_max(size)
+                index_list = []
+                # it will print number indexes where maximum values are founds
+                index_list = self.find_index(size)
+                # It will print date of indexes
+                index_date = []
+                for i in range(len(index_list)):
+                    index_date.append(str(self.find_date(index_list[i], path)))
+        return maximum
     
-    def find(self, index, path):
+    # Return the date at inputed index
+    def find_date(self, index, path):
         li,li1  = [],[]
         with open(path) as input_file:
-            reader = csv.reader(input_file)
             lines = input_file.readlines()
             for line in lines[2:]:
                 if line != '\n' and line.find('<') < 0:
@@ -43,20 +60,19 @@ class Weather:
         val = li[index]    
         return val
 
-
     def find_max(self,li):
         maximum = 0
         for i in range(len(li)):
             if maximum <= li[i]:
                 maximum = li[i]
         return maximum
-    
 
+    # Return the index of maximum value
     def find_index(self, li):
-        Maximum = self.find_max(li)
+        maximum = self.find_max(li)
         index_list = []
         for i,j in enumerate(li):
-            if j == Maximum:
+            if j == maximum:
                 index_list.append(i)
         return index_list
     
@@ -70,7 +86,13 @@ if __name__ == "__main__":
     for filename in os.listdir(DATA_DIR):
         file = os.path.join(DATA_DIR, filename)
         
-        max_list.append(weather.read(file, filename))
+        max_list.append(weather.read_file(file, filename))
         max_temp = max(max_list)
         
-    print("The Hottset is: ", max_temp)
+    # After uper loop is terminate the second loop will run again to find the max date , filename
+    for filename in os.listdir(DATA_DIR):
+        file = os.path.join(DATA_DIR, filename)
+        if max_temp == weather.read_file(file,filename):
+            max_temp_list.append(weather.read(file, filename))
+        
+    print(f"The Hottset is: {max_temp} at {max_temp_list}")
