8bf71eff9ffd16d7b30a487711bb9988a046d48f
Us-manArshad <Usmangani908.ua@gmail.com>
2019-04-12 00:48:22 -0700

    added features to search minimum temp and of it's months

--- main.py
+++ main.py
@@ -3,25 +3,20 @@ import os
 
 
 class Weather():
-    
-    # Method to find maximum values from list
-    def search_max(self, li):
-        max_val = 0
-        for i in range(len(li)):
-            if max_val < li[i]:
-                max_val = li[i]
-        return max_val
 
 
     # Method to find hottest temperature from all files
-    def search_all_files(self, DATA_DIR, max_temp):
-        months_list = []
+    def search_all_files(self, DATA_DIR, max_temp, min_temp):
+        max_months_list = []
+        min_months_list = []
         for filename in os.listdir(DATA_DIR):
             file = os.path.join(DATA_DIR, filename)
-            (max_temp_val, data_list, filename) = weather.read_files(file, filename)
+            (max_temp_val, data_list, filename, min_temp_val) = weather.read_files(file, filename)
             if max_temp == max_temp_val:
-                months_list.append(filename)
-        return months_list
+                max_months_list.append(filename)
+            if min_temp == min_temp_val:
+                min_months_list.append(filename)
+        return max_months_list, min_months_list
 
 
     # Read files and print maximum value of inputed weather month
@@ -35,32 +30,39 @@ class Weather():
                     if data[1] != '':
                         data_list.append(int(data[1]))
             # Find the maximum temp from Max TempC 
-            max_temp_val=0
+            max_temp_val = 0
+            min_temp_val = 0
             for i in range(len(data_list[:])):
                 if data_list[i] == '':
                     pass
                 else:
                     max_temp_val = max(data_list)
-        return max_temp_val, data_list, filename
+                    min_temp_val = min(data_list)
+                    
+        return max_temp_val, data_list, filename, min_temp_val
 
        
 if __name__ == "__main__":
     # Weather class object 
     weather = Weather() 
-    temp_list = []
+    max_temp_list = []
+    min_temp_list = []
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     DATA_DIR = os.path.join(ROOT_DIR, 'csv')
     for filename in os.listdir(DATA_DIR):
         file = os.path.join(DATA_DIR, filename)
 
-        (max_temp_val, data_list, filename) = weather.read_files(file, filename)
-        temp_list.append(max_temp_val)
-        max_temp = max(temp_list)
-
+        (max_temp_val, data_list, filename, min_temp_val) = weather.read_files(file, filename)
+        max_temp_list.append(max_temp_val)
+        min_temp_list.append(min_temp_val)
+        max_temp = max(max_temp_list)
+        min_temp = min(min_temp_list)
         print(f"List of Temperature of Month {filename} is :""\n" f"{data_list}")
-        print(f"Hottest Temperature in {filename} is : {max_temp_val}","\n")
+        print(f"Hottest Temperature in {filename} is : {max_temp_val} and the minimum temp is {min_temp_val} \n")
 
     # To find month of hottest temperature.
-    months_list = weather.search_all_files(DATA_DIR, max_temp)
-    print("\n!------------------------------Maximum Temperature-----------------------------------------!")
-    print(f"The maximum temperature of all the files data is: {max_temp} at months: \n{months_list}")
+    (max_months_list,min_months_list) = weather.search_all_files(DATA_DIR, max_temp, min_temp)
+    print("\n\n!----------------------------Maximum Temperature------------------------------!")
+    print(f"The maximum temperature of all the files data is: {max_temp} at months: \n{max_months_list}")
+    print(f"The maximum temperature of all the files data is: {min_temp} at months: \n{min_months_list} \n")
+    print("Attention: Those fields which are empty are not included for transparency of data. !! Thank you")
