a655dbc80f467f23c545edcab7036f7436b24119
Us-manArshad <Usmangani908.ua@gmail.com>
2019-04-11 22:31:21 -0700

    Removed one extra function Read_files()

--- main_csv.py
+++ main_csv.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from datetime import datetime
 
 
@@ -8,6 +9,7 @@ class Weather:
     def read(self, path, filename):
         size  = []
         with open(path) as input_file:
+            reader = csv.reader(input_file)
             lines = input_file.readlines()
             for line in lines[2:]:
                 if line != '\n' and line.find('<') < 0:
@@ -16,6 +18,7 @@ class Weather:
                         size.append(int(row[1]))
                 #find max from li size
                 maximum = self.find_max(size)
+
                 index_list = []
                 # it will print number indexes where maximum values are founds
                 index_list = self.find_index(size)
@@ -23,34 +26,13 @@ class Weather:
                 index_date = []
                 for i in range(len(index_list)):
                     index_date.append(str(self.find_date(index_list[i], path)))
-                
-        return index_date, filename
+        return index_date, filename, maximum
 
 
-    def read_file(self, path, filename):
-        size  = []
-        with open(path) as input_file:
-            lines = input_file.readlines()
-            for line in lines[2:]:
-                if line != '\n' and line.find('<') < 0:
-                    row = line.strip().split(',')
-                    if row[1] != '':
-                        size.append(int(row[1]))
-                #find max from li size
-                maximum = self.find_max(size)
-                index_list = []
-                # it will print number indexes where maximum values are founds
-                index_list = self.find_index(size)
-                # It will print date of indexes
-                index_date = []
-                for i in range(len(index_list)):
-                    index_date.append(str(self.find_date(index_list[i], path)))
-        return maximum
-    
-    # Return the date at inputed index
     def find_date(self, index, path):
         li,li1  = [],[]
         with open(path) as input_file:
+            reader = csv.reader(input_file)
             lines = input_file.readlines()
             for line in lines[2:]:
                 if line != '\n' and line.find('<') < 0:
@@ -60,6 +42,7 @@ class Weather:
         val = li[index]    
         return val
 
+
     def find_max(self,li):
         maximum = 0
         for i in range(len(li)):
@@ -67,7 +50,7 @@ class Weather:
                 maximum = li[i]
         return maximum
 
-    # Return the index of maximum value
+    
     def find_index(self, li):
         maximum = self.find_max(li)
         index_list = []
@@ -80,19 +63,23 @@ class Weather:
 if __name__ == "__main__":
     weather = Weather()
     max_list = []
-    max_temp_list = []
+    max_temp_date = []
+    max_file_name = []
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     DATA_DIR = os.path.join(ROOT_DIR, 'csv')
     for filename in os.listdir(DATA_DIR):
         file = os.path.join(DATA_DIR, filename)
+        (index_date, filename, maximum) = weather.read(file, filename)
         
-        max_list.append(weather.read_file(file, filename))
+        max_list.append(maximum)
         max_temp = max(max_list)
-        
+
     # After uper loop is terminate the second loop will run again to find the max date , filename
     for filename in os.listdir(DATA_DIR):
         file = os.path.join(DATA_DIR, filename)
-        if max_temp == weather.read_file(file,filename):
-            max_temp_list.append(weather.read(file, filename))
+        (index_date, filename, maximum) = weather.read(file, filename)
+        if max_temp == maximum:
+            max_temp_date.append(index_date)
+            max_file_name.append(filename)
         
-    print(f"The Hottset is: {max_temp} at {max_temp_list}")
+    print(f"The Hottset Temperature is: {max_temp} at day: {max_temp_date},\nfilenames: {max_file_name} Respectively.")
